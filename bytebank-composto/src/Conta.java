22da21e901b98cfefd94eb858bbf4820aaa7ee83
Cesar Moreschi <cesarmoreschi@gmail.com>
2021-12-23 16:22:47 -0300

    Inclusão de novos itens dos exercícios do curso de Orientação a Obejtos em Java.

--- bytebank-composto/src/Conta.java
+++ bytebank-composto/src/Conta.java
@@ -1,8 +1,15 @@
 public class Conta{
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public Conta (int agencia, int numero) {
+		Conta.total++;
+		this.agencia = agencia;
+		this.numero = numero;
+	}
 	
 	public void depositar(double valor){
 		this.saldo += valor;
@@ -29,5 +36,35 @@ public class Conta{
 	
 	public double getSaldo() {
 		return this.saldo;
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		if(agencia >= 0) {
+			this.agencia = agencia;
+		}
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
 	}
 }
--- bytebank-composto/src/TestaBanco.java
+++ bytebank-composto/src/TestaBanco.java
@@ -2,7 +2,7 @@
 
 public class TestaBanco {
 	public static void main(String[] args) {
-		Cliente paulo = new Cliente();
+		/*Cliente paulo = new Cliente();
 		paulo.nome = "Paulo Silveira";
 		paulo.cpf = "222.222.222-22";
 		paulo.profissao = "Programador";
@@ -14,7 +14,7 @@ public class TestaBanco {
 		
 		System.out.println(contaDoPaulo.titular.nome);
 		
-		System.out.println(contaDoPaulo.getSaldo());
+		System.out.println(contaDoPaulo.getSaldo());*/
 	}
 
 }
